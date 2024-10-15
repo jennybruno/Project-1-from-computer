b48420d6d6f4475ef4f54f2cddf29b197939ae93
Jenny Bruno <jennybruno@192.168.1.54>
2024-10-15 05:43:43 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const toggle = document.getElementById('toggle');
+const nav = document.getElementById('active');
+
+
+toggle.addEventListener('click', () => {
+   nav.classList.toggle('active')
+});
